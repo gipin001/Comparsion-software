87706fd49a87e58f303f25efc8a123b80ed7a8b5
Gipin George <96596240+gipin001@users.noreply.github.com>
2022-10-14 11:41:34 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace hashvalue2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(469, 203);
+            this.progressBar2.MarqueeAnimationSpeed = 50;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(278, 31);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 245);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button3.Location = new System.Drawing.Point(561, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "compute";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.WorkerReportsProgress = true;
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(469, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "-";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(177, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(495, 61);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Binary Bit comparison of two files";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Info;
+            this.button4.Location = new System.Drawing.Point(799, 306);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 39);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Location = new System.Drawing.Point(99, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(276, 25);
+            this.label3.TabIndex = 10;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.Location = new System.Drawing.Point(99, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(277, 22);
+            this.label4.TabIndex = 11;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 387);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace hashvalue2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string e2;
+        public string e3;
+        //string folderPath = "";
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string fPath = e.Argument.ToString();
+            byte[] buffer;
+            int bytesRead;
+            long size;
+            long totalBytesRead = 0;
+            using (Stream file = File.OpenRead(fPath))
+            {
+                size = file.Length;
+                using (HashAlgorithm hasher = MD5.Create())
+                {
+                    do
+                    {
+                        buffer = new byte[4096];
+                        bytesRead = file.Read(buffer, 0, buffer.Length);
+                        totalBytesRead += bytesRead;
+                        hasher.TransformBlock(buffer, 0, bytesRead, null, 0);
+                        backgroundWorker1.ReportProgress((int)((double)totalBytesRead / size * 100));
+                    }
+                    while (bytesRead != 0);
+
+                    hasher.TransformFinalBlock(buffer, 0, 0);
+                    e.Result = MakeHashString(hasher.Hash);
+                }
+            }
+        }
+        private static string MakeHashString(byte[] hashBytes)
+        {
+            StringBuilder hash = new StringBuilder(32);
+            foreach (byte b in hashBytes)
+            {
+                hash.Append(b.ToString("X2").ToLower());
+            }
+            return hash.ToString();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            e2 = e.Result.ToString();
+            //Console.WriteLine(typeof(e2));
+            //MessageBox.Show(e2);
+            backgroundWorker2.RunWorkerAsync(textBox2.Text);
+
+            //progressBar1.Value = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder Selection.";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowser.FileName;
+                textBox1.Text = folderPath;
+                label3.Text = "OK";
+                label3.BackColor = Color.Green;
+                // ...
+            }
+            //progressBar1.Value = 1;
+
+            //backgroundWorker1.RunWorkerAsync(textBox1.Text);
+        }
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string fPath = e.Argument.ToString();
+            byte[] buffer;
+            int bytesRead;
+            long size;
+            long totalBytesRead = 0;
+
+            using (Stream file = File.OpenRead(fPath))
+            {
+                size = file.Length;
+                using (HashAlgorithm hasher = MD5.Create())
+                {
+                    do
+                    {
+                        buffer = new byte[4096];
+                        bytesRead = file.Read(buffer, 0, buffer.Length);
+                        totalBytesRead += bytesRead;
+                        hasher.TransformBlock(buffer, 0, bytesRead, null, 0);
+                        backgroundWorker2.ReportProgress((int)((double)totalBytesRead / size * 100));
+                    }
+                    while (bytesRead != 0);
+
+                    hasher.TransformFinalBlock(buffer, 0, 0);
+                    e.Result = MakeHashString(hasher.Hash);
+                }
+            }
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar2.Value = e.ProgressPercentage;
+                
+                 label1.Text = (e.ProgressPercentage).ToString() + "%";
+           
+
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+            e3 = e.Result.ToString();
+            //MessageBox.Show(e3);
+            Console.WriteLine(e3);
+            
+            //progressBar2.Value = 100;
+            if (e2 == e3)
+            {
+              //MessageBox.Show("equal");
+                label1.Text = "EXACT SAME";
+                label1.BackColor = Color.Green;
+                //progressBar2.Value = 100;
+            }
+            else
+            {
+                //MessageBox.Show("not equal");
+                label1.Text = "NOT COMPARABLE";
+                
+                label1.BackColor = Color.Red;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            backgroundWorker1.RunWorkerAsync(textBox1.Text);
+            
+            //Console.WriteLine(e2);
+            //Console.WriteLine(e2);
+            //
+            //Console.WriteLine(e3);
+
+            //if (e2==e3)
+            //{
+            //    MessageBox.Show("equal");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("not equal");
+            //}
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder Selection.";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowser.FileName;
+                textBox2.Text = folderPath;
+                label4.Text = "OK";
+                label4.BackColor = Color.Green;
+                // ...
+            }
+            
+            //backgroundWorker2.RunWorkerAsync(textBox2.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+
+    }
+}
